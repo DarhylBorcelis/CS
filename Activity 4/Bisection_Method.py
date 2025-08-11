9424a872d66a9f2506a1e3d9338519f564e0c2e0
DarhylBorcelis <darhylborcelis002@gmail.com>
2025-08-11 11:59:30 +0100

    Update Bisection_Method.py

--- Activity 4/Bisection_Method.py
+++ Activity 4/Bisection_Method.py
@@ -1,27 +1,37 @@
-print("")
-print("***Numerical Analysis → Root-Finding Algorithms***")
-
-tolerance = float(input("Tolerance only start from 0.01: "))
-a = float(input("Start a: "))
-b = float(input("Start b: "))
-i = 0
+import os
 
 
 def f(x):
     return x**3 - x - 2
 
 
-if f(a) * f(b) >= 0:
-    print("No root in this range.")
-else:
-    while True:
-        i += 1
-        c = (a + b) / 2
-        fc = f(c)
-        if abs(fc) < tolerance:
-            print(f"Root: {c:.6f} in {i} iterations.")
-            break
-        if f(a) * fc < 0:
-            b = c
-        else:
-            a = c
+while True:
+    print("")
+    print("***Numerical Analysis → Root-Finding Algorithms***")
+
+    tolerance = float(input("Tolerance only start from 0.01: "))
+    a = float(input("Start a: "))
+    b = float(input("Start b: "))
+    i = 0
+
+    if f(a) * f(b) >= 0:
+        print("No root in this range.")
+    else:
+        while True:
+            i += 1
+            c = (a + b) / 2
+            fc = f(c)
+            if abs(fc) < tolerance:
+                print(f"Root: {c:.6f} in {i} iterations.")
+                break
+            if f(a) * fc < 0:
+                b = c
+            else:
+                a = c
+
+    con = input("Repeat? y/n: ").lower()
+    if con == "n":
+        break
+    else:
+        os.system('cls')
+
